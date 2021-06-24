b7ce1e5014104890e16198f750453519db3d6a1e
scad89 <71154471+scad89@users.noreply.github.com>
2021-06-24 14:58:56 +0300

    recreate models

--- your_finance_helper/main_page/models.py
+++ your_finance_helper/main_page/models.py
@@ -3,36 +3,36 @@ from datetime import date
 
 
 class Section(models.Model):
-    section = models.CharField('Раздел', max_length=25)
+    section = models.CharField('Section', max_length=25)
 
     def __str__(self):
         return self.section
 
     class Meta:
-        verbose_name = 'Новость'
-        verbose_name_plural = 'Новости'
+        verbose_name = 'Section'
+        verbose_name_plural = 'Sections'
 
 
 class Category(models.Model):
-    category = models.CharField('Категория', max_length=25, default='')
+    category = models.CharField('Category', max_length=25, blank=True)
 
     def __str__(self):
         return self.category
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
 
 class NameOperation(models.Model):
-    name = models.CharField('Наименование', max_length=25, default='')
+    name = models.CharField('Name', max_length=25, blank=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Наименование'
-        verbose_name_plural = 'Наименования'
+        verbose_name = 'Name'
+        verbose_name_plural = 'Names'
 
 
 class GeneralTable(models.Model):
@@ -41,21 +41,21 @@ class GeneralTable(models.Model):
         OUTCOME = 'OUT', ('Outcome')
 
     type_of_transaction = models.CharField(
-        choices=TypeOfTransaction.choices, default=TypeOfTransaction.OUTCOME, max_length=3)
+        choices=TypeOfTransaction.choices, max_length=3)
     d_section = models.ForeignKey(
-        Section, on_delete=models.SET_NULL, null=True, verbose_name='Раздел')
+        Section, on_delete=models.SET_NULL, null=True, verbose_name='Section')
     id_category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, verbose_name='Категория')
+        Category, on_delete=models.SET_NULL, null=True, verbose_name='Category')
     id_name = models.ForeignKey(
-        NameOperation, on_delete=models.SET_NULL, null=True, verbose_name='Наименование')
-    sum_money = models.DecimalField('Сумма', decimal_places=2, max_digits=10)
-    date = models.DateField('Дата', default=date.today)
-    comment = models.CharField('Комментарии', max_length=100, default='')
-    on_delete = models.BooleanField('enabled', default=False)
+        NameOperation, on_delete=models.SET_NULL, null=True, verbose_name='Name')
+    sum_money = models.DecimalField('Amount', decimal_places=2, max_digits=10)
+    date = models.DateField('Date', default=date.today)
+    comment = models.CharField('Comment', max_length=100, blank=True)
+    enabled = models.BooleanField('Enabled', default=False)
 
     def __str__(self):
         return self.type_of_transaction
 
     class Meta:
-        verbose_name = 'Главная таблица'
-        verbose_name_plural = 'Главные таблицы'
+        verbose_name = 'General Table'
+        verbose_name_plural = 'General Tables'
