c462f961dbd6fdf5c341ba37c557ff250d520508
Niccolum <lastsal@mail.ru>
2021-07-18 15:56:30 +0300

    Revert "Implementation of the section Current financial results and reports"

--- your_finance_helper/your_finance_helper/urls.py
+++ your_finance_helper/your_finance_helper/urls.py
@@ -20,6 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_page.urls')),
-    path('reports/', include('reports.urls'))
+    path('', include('main_page.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
