8e2a39704a74c83fc0e990d7a8347d29edf17813
scad89 <71154471+scad89@users.noreply.github.com>
2021-07-18 14:03:04 +0300

    create update and delete in reports for current opeartion, add button back

--- your_finance_helper/reports/views.py
+++ your_finance_helper/reports/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from main_page.models import *
-from django.views.generic import View
+from django.urls import reverse_lazy
+from django.views.generic import View, DetailView, ListView, UpdateView
+from .forms import *
 
 
 class ReportsButtonsView(View):
@@ -8,10 +10,32 @@ class ReportsButtonsView(View):
         return render(request, 'reports/reports.html')
 
 
-class DetailedCurrentFinancialResultsView(View):
+class DetailedCurrentFinancialResultsView(ListView):
     def get(self, request):
-        income_all = GeneralTable.objects.values('id_section__section', 'id_category__category', 'id_name__name', 'sum_money',
-                                                 'currency', 'date', 'comment').order_by('date').filter(type_of_transaction='IN').filter(enabled=False)
-        outcome_all = GeneralTable.objects.values('id_section__section', 'id_category__category', 'id_name__name', 'sum_money',
-                                                  'currency', 'date', 'comment').order_by('date').filter(type_of_transaction='OUT').filter(enabled=False)
+        income_all = GeneralTable.objects.order_by('date').filter(
+            type_of_transaction='IN').filter(enabled=False)
+        outcome_all = GeneralTable.objects.order_by('date').filter(
+            type_of_transaction='OUT').filter(enabled=False)
         return render(request, 'reports/detailed_current_financial_results.html', {'income_all': income_all, 'outcome_all': outcome_all})
+
+
+class TransactionView(DetailView):
+    model = GeneralTable
+    template_name = 'reports/transaction_view.html'
+    context_object_name = 'transaction'
+
+
+class TransactionUpdateView(UpdateView):
+    model = GeneralTable
+    template_name = 'reports/transaction_update.html'
+    form_class = TransactionUpdateForm
+    context_object_name = 'transaction_form'
+    success_url = reverse_lazy('detailed_current_financial_results')
+
+
+class TransactionDeleteView(UpdateView):
+    model = GeneralTable
+    template_name = 'reports/transaction_delete.html'
+    form_class = TransactionDeleteForm
+    context_object_name = 'transaction_d_form'
+    success_url = reverse_lazy('detailed_current_financial_results')
