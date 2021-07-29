5820cb806522b886ae9d9bf7886eb860b9964fb9
scad89 <71154471+scad89@users.noreply.github.com>
2021-07-29 04:06:45 +0300

    corrected view for main_page

--- your_finance_helper/main_page/views.py
+++ your_finance_helper/main_page/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Section, Category, NameOperation, GeneralTable
+from .models import GeneralTable
 from django.db.models import Sum
 from django.views.generic import View, CreateView
 from django.urls import reverse_lazy
@@ -8,23 +8,35 @@ from .forms import AddIncomeForm, AddOutcomeForm, AddNewSectionForm, AddNewCateg
 
 class IndexView(View):
     def get(self, request):
-        in_section = GeneralTable.objects.values(
-            'id_section__section', 'id_section__id').annotate(sum=Sum('sum_money')).filter(type_of_transaction='IN').filter(enabled=False)
-        in_category = GeneralTable.objects.values(
-            'id_category__id', 'id_category__category', 'id_category__to_section').distinct().filter(id_category__isnull=False).filter(type_of_transaction='IN').filter(enabled=False)
-        in_name = GeneralTable.objects.values(
-            'id_name__name', 'id_name__to_category').annotate(sum=Sum('sum_money')).filter(id_name__isnull=False).filter(type_of_transaction='IN').filter(enabled=False)
-        in_all_sum = GeneralTable.objects.filter(type_of_transaction='IN').filter(
-            enabled=False).aggregate(in_money=Sum('sum_money'))
-        out_section = GeneralTable.objects.values(
-            'id_section__section', 'id_section__id').annotate(sum=Sum('sum_money')).filter(type_of_transaction='OUT').filter(enabled=False)
-        out_category = GeneralTable.objects.values(
-            'id_category__id', 'id_category__category', 'id_category__to_section').distinct().filter(id_category__isnull=False).filter(type_of_transaction='OUT').filter(enabled=False)
-        out_name = GeneralTable.objects.values(
-            'id_name__name', 'id_name__to_category').annotate(sum=Sum('sum_money')).filter(id_name__isnull=False).filter(type_of_transaction='OUT').filter(enabled=False)
-        out_all_sum = GeneralTable.objects.filter(type_of_transaction='OUT').filter(
-            enabled=False).aggregate(out_money=Sum('sum_money'))
-        return render(request, 'main_page/index.html', {'in_section': in_section, 'in_category': in_category, 'in_name': in_name, 'out_section': out_section, 'out_category': out_category, 'out_name': out_name, 'in_all_sum': in_all_sum, 'out_all_sum': out_all_sum})
+        def split_queryset(data):
+            sum_all = 0
+            dict_section = {}
+            dict_category = {}
+            dict_name = {}
+            for i in data:
+                dict_section[i['id_section__section']] = {
+                    'id_section__id': i['id_section__id']}
+                dict_category[i['id_category__category']] = {
+                    'id_category__id': i['id_category__id'], 'id_category__to_section': i['id_category__to_section']}
+                dict_name[i['id_name__name']] = {
+                    'id_name__to_category': i['id_name__to_category'], 'sum': i['sum']}
+                sum_all += i['sum']
+            return dict_section, dict_category, dict_name, sum_all
+
+        in_dict_section, in_dict_category, in_dict_name, in_sum_all = split_queryset(
+            GeneralTable.objects.select_related('id_section__id', 'id_section__section', 'id_category__id', 'id_category__category',
+                                                'id_category__to_section', 'id_name__name', 'id_name__to_category').filter(type_of_transaction='IN').values(
+                'id_section__id', 'id_section__section', 'id_category__id', 'id_category__category', 'id_category__to_section',
+                'id_name__name', 'id_name__to_category').annotate(sum=Sum('sum_money')).order_by('id_section'))
+
+        out_dict_section, out_dict_category, out_dict_name, out_sum_all = split_queryset(
+            GeneralTable.objects.select_related('id_section__id', 'id_section__section', 'id_category__id', 'id_category__category',
+                                                'id_category__to_section', 'id_name__name', 'id_name__to_category').filter(type_of_transaction='OUT').values(
+                'id_section__id', 'id_section__section', 'id_category__id', 'id_category__category', 'id_category__to_section',
+                'id_name__name', 'id_name__to_category').annotate(sum=Sum('sum_money')).order_by('id_section'))
+        return render(request, 'main_page/test.html', {'in_dict_section': in_dict_section, 'in_dict_category': in_dict_category,
+                                                       'in_dict_name': in_dict_name, 'in_sum_all': in_sum_all, 'out_dict_section': out_dict_section, 'out_dict_category': out_dict_category,
+                                                       'out_dict_name': out_dict_name, 'out_sum_all': out_sum_all})
 
 
 class AddIncomeView(CreateView):
