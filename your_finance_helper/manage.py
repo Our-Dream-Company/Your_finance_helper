a88fad5631b5f03154edfcf5d7bd5a6fcb45148c
scad89 <71154471+scad89@users.noreply.github.com>
2021-08-29 00:53:36 +0300

    add .coveragerc

--- your_finance_helper/manage.py
+++ your_finance_helper/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_finance_helper.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'your_finance_helper.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
