afed6187693652876af46b8821ecc4e87cd48833
scad89 <71154471+scad89@users.noreply.github.com>
2021-12-04 21:10:11 +0300

    corrected name template auth, add new template

--- your_finance_helper/your_finance_helper/settings.py
+++ your_finance_helper/your_finance_helper/settings.py
@@ -177,4 +177,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
-LOGIN_URL = '/authentication/login_in'
+LOGIN_URL = '/authentication/login'
