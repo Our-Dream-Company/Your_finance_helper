f16326b62b8e63e2625acf61365d6f1cf810c91d
scad89 <71154471+scad89@users.noreply.github.com>
2021-12-18 20:38:13 +0300

    delete comment code

--- your_finance_helper/your_finance_helper/settings.py
+++ your_finance_helper/your_finance_helper/settings.py
@@ -15,9 +15,6 @@ import os
 from pathlib import Path
 
 load_dotenv()
-# env_path = Path('.')/'.env'
-# load_dotenv(dotenv_path=env_path)
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
