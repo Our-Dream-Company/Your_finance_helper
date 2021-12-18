9323d68c582493a26483c9c13f71ec6d6bc42fd5
scad89 <71154471+scad89@users.noreply.github.com>
2021-12-18 21:00:14 +0300

    add --no-input

--- your_finance_helper/entrypoint.sh
+++ your_finance_helper/entrypoint.sh
@@ -4,6 +4,6 @@ python manage.py makemigrations
 
 python manage.py migrate 
 
-python manage.py collectstatic 
+python manage.py collectstatic --no-input
 
 gunicorn your_finance_helper.wsgi:application --bind 0.0.0.0:8000 --reload -w 4
