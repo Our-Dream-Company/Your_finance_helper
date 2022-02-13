dc9f502f406d836c68e2bd69a5bb283ddbdff399
scad89 <71154471+scad89@users.noreply.github.com>
2022-02-13 04:27:15 +0300

    create DIR config, change settings for pytest

--- your_finance_helper/manage.py
+++ your_finance_helper/manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'your_finance_helper.settings')
+                          'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
